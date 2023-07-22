d45eea818c807d3296b4a043878bdc469faed8f1
Yves Bresson <yv.bres@gmail.com>
2023-07-22 10:20:23 +0200

    correct ex10

--- ex01-10/ex10.py
+++ ex01-10/ex10.py
@@ -5,32 +5,40 @@ import polars as pl
 import numpy as np
 from polars.testing import assert_series_equal
 
+for _ in range(1_000):  # try multiple times due to randomness
 
-vals = np.random.randint(1, 5, [200])
+    vals = np.random.randint(1, 5, [100])
 
-# pandas
-ser = pd.Series(vals)
-mask = ser.isin(ser.value_counts().head(2).index)
-pd_ser = ser.where(mask, "Other")
-pd_ser.name = "new"
+    # pandas
+    pd_ser = pd.Series(vals)
+    # to handle case of 3 most frequent, we sort by both count and value
+    counts = (
+        pd_ser
+        .value_counts().to_frame()
+        .reset_index(names=["val"])
+        .sort_values(by=["count", "val"], ascending=False)
+    )
+    mask = pd_ser.isin(counts["val"].head(2))
+    pd_ser = pd_ser.where(mask, "Other")
+    pd_ser.name = "new"
 
-# polars
-ser = pl.Series("new", vals)
-mask = ser.is_in(
-    ser
-    .value_counts()  # transform to DataFrame
-    .sort(by="counts", descending=True)
-    .limit(2)
-    ["new"]  # transform to Series
-)
-expr = (
-    pl.when(mask)
-    .then(ser)
-    .otherwise("Other")
-    .alias("new")
-)
-pl_ser = ser.to_frame().with_columns(expr)  # DataFrame
+    # polars
+    pl_ser = pl.Series("new", vals)
+    mask = pl_ser.is_in(
+        pl_ser
+        .value_counts()  # transform to DataFrame
+        .sort(by=["counts", "new"], descending=True)
+        .limit(2)
+        ["new"]  # transform to Series
+    )
+    expr = (
+        pl.when(mask)
+        .then(pl_ser)
+        .otherwise("Other")
+        .alias("new")
+    )
+    pl_ser = pl_ser.to_frame().with_columns(expr)  # DataFrame
 
+    assert_series_equal(pl.from_pandas(pd_ser.astype(str)), pl_ser["new"])
 
-assert_series_equal(pl.from_pandas(pd_ser.astype(str)), pl_ser["new"])
 print("Completed.")
